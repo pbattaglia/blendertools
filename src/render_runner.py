14cf39299b9bda6c73c5e2ef2027bf7883c7d659
Peter Battaglia <pbatt@mit.edu>
2013-03-16 23:33:06 -0400

    Lots of adjustments and cleanup. Passed basic tests.

--- src/render_runner.py
+++ src/render_runner.py
@@ -1,8 +1,12 @@
 """ Controls Blender rendering."""
 import argparse
-import bpy
+try:
+    import bpy
+except ImportError:
+    bpy = None
 import inspect
 import os
+import signal
 import sys
 #
 from pdb import set_trace as BP
@@ -12,16 +16,19 @@ class DeviceError(Exception):
     pass
 
 
+def kill_blender():
+    """ Kill Blender process."""
+    os.kill(os.getpid(), signal.SIGUSR1)
+
+
 gpu_devices = ("CUDA", "OPENCL")
 
 
-def run(device_t=None, scenes=False, samples=None):
+def blender_run(f_anim, device_t=None, scenes=False, samples=None, frame=None):
     """ Run rendering procedures."""
 
-    def render(scene, device, samples):
+    def render(f_anim, scene, device, samples, frame):
         """ Set the scene and do the render."""
-        # Make scene active.
-        bpy.context.screen.scene = scene
         # Set the rendering device.
         scene.cycles.device = device
         # Print device setting.
@@ -29,19 +36,14 @@ def run(device_t=None, scenes=False, samples=None):
         # Set number of samples.
         if samples:
             scene.cycles.samples = samples
-        # # Set start and end frames for animation.
-        # if frames:
-        #     scene.frame_start = max(frames[0], scene.frame_start)
-        #     f1 = (frames[1] < 0) * (scene.frame_end + 1) + frames[1]
-        #     scene.frame_end = min(f1, scene.frame_end)
+        # Set frame.
+        if frame is not None:
+            scene.frame_current = frame
         # Set output path.
         scene.render.filepath = "//%s.png" % scene.name
         # Render.
-        # bpy.ops.render.render(animation=frames, write_still=not frames)
-        bpy.ops.render.render(animation=False, write_still=True)
-        # Print device/setting again after rendering.
-        print(bcups.compute_device_type, bcups.compute_device,
-              scene.cycles.device)
+        bpy.ops.render.render(animation=f_anim, write_still=not f_anim,
+                              scene=scene.name)
 
     # Local nicknames.
     bcups = bpy.context.user_preferences.system
@@ -63,17 +65,22 @@ def run(device_t=None, scenes=False, samples=None):
             raise DeviceError("Failed to set compute device: CPU")
         device = "CPU"
     # Make the list of scenes.
-    if scenes == []:
-        scenes = [bpy.context.scene]
+    if scenes:
+        if len(scenes) == 1:
+            scenes = [bpy.data.scenes[scenes[0]]]
+        else:
+            scenes = bpy.data.scenes[slice(*scenes)]
     else:
-        scenes = bpy.data.scenes[slice(*scenes)]
-    # Loop over scenes
+        scenes = [bpy.context.scene]
+    # Loop over scenes.
     for scene in scenes:
-        render(scene, device, samples)
+        render(f_anim, scene, device, samples, frame)
 
 
-if __name__ == "__main__":
-    ## Cmd line interface.
+def run():
+    """ Wrap all functionality in run() function to handle exceptions
+    without going to Blender."""
+
     def is_int(s):
         """ Returns bool indicating whether s can be converted into an int."""
         try:
@@ -92,40 +99,98 @@ if __name__ == "__main__":
             X = None
         return X
 
-    # Get the input arguments.
-    # All args.
-    args0 = sys.argv
-    # Relevant arguments.
-    if "--" in args0:
-        idx = args0.index("--")
-    else:
-        # Figure out which argument this script was.
+    # Get first Python argument index: idx.
+    try:
+        idx = sys.argv.index("--") + 1
+    except ValueError:
+        # "--" isn't an argument, start with next argument after this script.
+        # Determine which argument this script is.
         thisfile = os.path.basename(inspect.getfile(inspect.currentframe()))
-        idx = args0.index(thisfile)
-    args = args0[idx + 1:]
+        idx = None
+        for i, a in enumerate(sys.argv):
+            if os.path.basename(a) == thisfile:
+                idx = i + 1
+        if idx is None:
+            raise argparse.ArgumentError("Cannot split argument list.")
+    # The Python script's arguments.
+    args = sys.argv[idx:]
+    # Description for parser.
+    try:
+        description = __doc__
+    except NameError:
+        description = ""
     # Parser object.
-    parser = argparse.ArgumentParser(description="render_runner arguments.")
-    # 'device' argument.
+    parser = argparse.ArgumentParser(description=description)
+    # Argument: f_anim.
+    parser.add_argument(
+        "--render-anim", "-a", action="store_true", default=False,
+        help="Render frames from start to end.")
+    # Argument: device.
     devices = gpu_devices + ("CPU",)
     parser.add_argument(
         "--device", "-D", choices=devices,
-        help="Specify compute device: (%s)" % (", ".join(devices)))
-    # 'samples' argument.
+        help="Sets compute device: (%s)" % (", ".join(devices)))
+    # Argument: samples.
     parser.add_argument(
         "--samples", default=None, type=int,
-        help="Specify how many samples per render.")
-    # 'scenes' argument.
+        help="Sets number of samples per render.")
+    # Argument: scenes.
     parser.add_argument(
         "--scenes", nargs="*", default=[], type=parse_scenes,
-        help="Specify which scenes to render. No argument means all.")
+        help="List of scene names or indices to render. Defaults to none.")
+    # Argument: frame.
+    parser.add_argument(
+        "--render-frame", "-f", default=None, type=int,
+        help="Sets frame to render.")
+    # Argument: f_no_kill.
+    parser.add_argument(
+        "--no-kill", action="store_true", default=False,
+        help="Will not kill Blender at the end of this script.")
     # Create parser and parse args.
     try:
-        parsed = parser.parse_args(args)
-    except:
-        print("Failed to parse arguments.")
-        BP()
+        parsed, remaining = parser.parse_known_args(args)
+    except SystemExit as err:
+        if not err.code:
+            # Normal exit occurred, probably from "--help". Just kill
+            # Blender now, because otherwise it will continue running
+            # (and probably print Blender's help to stdout).
+            kill_blender()
+        else:
+            # Abnormal exit.
+            # Re-raise error.
+            raise err
+            # Exit script and exit Blender.
+            sys.exit(err.code)
+            bpy.ops.wm.quit_blender()
+            BP()
+            kill_blender()
+    # Get parsed arguments.
+    f_anim = parsed.render_anim
     device_t = parsed.device
     samples = parsed.samples
     scenes = parsed.scenes
-    # Render.
-    run(device_t=device_t, samples=samples, scenes=scenes)
+    frame = parsed.render_frame
+    f_kill = not parsed.no_kill
+    BP()
+    if bpy:
+        # Render.
+        blender_run(f_anim, device_t=device_t, samples=samples, scenes=scenes,
+                    frame=frame)
+    else:
+        print("** Called from outside Blender. Exiting. **")
+    # Kill blender. This script is intended to be used as a final
+    # command line arguments to Blender because there's Really no good
+    # way to consume the Python arguments and then return to take more
+    # Blender arguments. Command line arg "--no-kill" can be used to
+    # avoid this.
+    if f_kill:
+        kill_blender()
+
+
+if __name__ == "__main__":
+    ## Cmd line interface.
+    try:
+        run()
+    except:
+        print("\n\nError generated from Python script.")
+        BP()
