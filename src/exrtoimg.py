f658b2e70366eb4cc9af5086591f70f836ac329e
Peter Battaglia <pbatt@mit.edu>
2013-04-09 11:19:15 -0400

    Added batch support and cleaned small bugs

--- src/exrtoimg.py
+++ src/exrtoimg.py
@@ -2,11 +2,14 @@
 """ Converts an .exr (OpenEXR format) file's depth channgel to a .png.
 http://www.blender.org/forum/viewtopic.php?p=98212&sid=4cef7d0c329b3c3fe366e66832482fc0
 
-TODO:
-- Add batch support.
+Peter Battaglia
+2013.04.08
 """
 # Standard
-import argparse
+from argparse import ArgumentError, ArgumentParser
+import glob
+from math import log10
+from operator import add
 import os
 import sys
 # External
@@ -45,16 +48,38 @@ def get_channels(exrimg, outchans="RGBA"):
     return channels
 
 
+def convert(infilename, outfilename, outchans, normalize, nanfill):
+    """ Convert one file."""
+    # Load the .exr file.
+    exrimg = load_exr(infilename)
+    # Get the channels.
+    channels = get_channels(exrimg, outchans=outchans)
+    # Compose image data into what is necessary for output.
+    outimg = np.squeeze(np.dstack(channels))
+    # Fix nans and infs.
+    badidx = ~np.isfinite(outimg)
+    if np.any(badidx):
+        outimg[badidx] = nanfill
+    # Normalize (optionally).
+    if normalize:
+        outimg /= outimg.max()
+        # Reset any nanfill values that were changed by normalizing.
+        # what was specified.
+        outimg[badidx] = nanfill
+    # Write output file.
+    imsave(outfilename, outimg)
+
+
 if __name__ == "__main__":
     # Parse input arguments.
 
     ## Cmd line interface.
     # Create parser object.
-    parser = argparse.ArgumentParser(description="Output options.")
+    parser = ArgumentParser(description="Output options.")
     # Argument: input file name.
-    parser.add_argument("infilename", help="Input filename.")
+    parser.add_argument("innames", nargs="+", help="Input filename(s).")
     # Argument: output file name.
-    parser.add_argument("-o", dest="outname",
+    parser.add_argument("-o", dest="outname", default="",
                         help=("Output file name. If extension is "
                               "present, it overrides the default."))
     # Argument: output file format.
@@ -74,39 +99,48 @@ if __name__ == "__main__":
                         help="Value to replace nans with.")
     # Create parser and parse args.
     parsed = parser.parse_args()
-    infilename = parsed.infilename
+    innames = reduce(add, [sorted(glob.glob(fn)) for fn in parsed.innames])
     outname = parsed.outname
-    if outname is None:
-        outname = os.path.splitext(infilename)[0]
     outfmt = parsed.outfmt
-    if outfmt is None:
-        deffmt = os.path.splitext(outname)[1].lstrip(".")
-        if deffmt:
-            outfmt = deffmt
-        else:
-            outfmt = "png"
     outchans = parsed.outchans
-    if parsed.Z:
-        outchans = "Z"
     normalize = parsed.normalize
     nanfill = parsed.nanfill
-    # Output filename.
-    outfilename = "%s.%s" % (outname, outfmt)
-    # Load the .exr file.
-    exrimg = load_exr(infilename)
-    # Get the channels.
-    channels = get_channels(exrimg, outchans=outchans)
-    # Compose image data into what is necessary for output.
-    outimg = np.squeeze(np.dstack(channels))
-    # Fix nans and infs.
-    badidx = ~np.isfinite(outimg)
-    if np.any(badidx):
-        outimg[badidx] = nanfill
-    # Normalize (optionally).
-    if normalize:
-        outimg /= outimg.max()
-        # Reset any nanfill values that were changed by normalizing.
-        # what was specified.
-        outimg[badidx] = nanfill
-    # Write output file.
-    imsave(outfilename, outimg)
+    ## Set parameters according to inputs.
+    N = len(innames)
+    if N == 0:
+        raise IOError("Cannot find input files that match: %s" %
+                      ", ".join(parsed.innames))
+    # Output format.
+    if outfmt is None:
+        # If no outfmt was input, try getting it from the outname.
+        outfmt = os.path.splitext(outname)[1].lstrip(".")
+        if not outfmt:
+            # If outname also doesn't specify outfmt, default to .png.
+            outfmt = "png"
+    # Output file's base name without extension.
+    outname = os.path.splitext(outname)[0]
+    # Output file names.
+    nametemplate = "%%s_%%0%dd" % (int(log10(N)) + 1)
+    outnames = []
+    for i, inname in enumerate(innames):
+        if not outname:
+            # If outname is empty, use the inname.
+            on = os.path.splitext(inname)[0]
+        else:
+            # If outname was supplied, use it and append count if
+            # there is more than one inname.
+            if N == 1:
+                on = outname
+            else:
+                on = nametemplate % (outname, i)
+        outnames.append("%s.%s" % (on, outfmt))
+    # If "-Z" flag was input, override outchans.
+    if parsed.Z:
+        outchans = "Z"
+    # Run the converter over all files.
+    for inname, outname in zip(innames, outnames):
+        # Make sure we're not overwriting the input file.
+        if inname == outname:
+            raise ValueError("Attempted to overwrite: %s" % inname)
+        # Do the conversion.
+        convert(inname, outname, outchans, normalize, nanfill)
