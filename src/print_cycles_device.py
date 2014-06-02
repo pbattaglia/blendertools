45306026c081724b82f3959cc68646fddb36c5e9
Peter Battaglia <pbatt@mit.edu>
2014-06-02 10:50:10 -0400

    Updating print_cycles_devices' output

--- src/print_cycles_device.py
+++ src/print_cycles_device.py
@@ -1,10 +1,21 @@
 """ Prints Cycles device."""
 import bpy
+from pdb import set_trace as BP
 
 
 if __name__ == "__main__":
     # Print device settings.
     bcups = bpy.context.user_preferences.system
-    print(bcups.compute_device_type, bcups.compute_device)
-    print(bpy.context.scene.cycles.device)
+    bcupsbp = bcups.bl_rna.properties
+    current_device_type = bcups.compute_device_type
+    device_types = bcupsbp['compute_device_type'].enum_items.keys()
+    devices = {}
+    print('Devices:')
+    for dt in device_types:
+        bcups.compute_device_type = dt
+        devices[dt] = bcupsbp['compute_device'].enum_items.keys()
+        print('  {}: {}'.format(dt, ', '.join(devices[dt])))
+    bcups.compute_device_type = current_device_type
+    # print(bcups.compute_device_type, bcups.compute_device)
+    # print(bpy.context.scene.cycles.device)
 
